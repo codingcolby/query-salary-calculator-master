edc86df2b1b25705158043413ff1a8343cd9892a
allthethings <allthethings@allthethingss-MacBook-Pro.local>
2020-03-13 17:07:12 -0500

    shaping the code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("js");
+
+
+function collect();
+function store();
+function clearInput();
+function append();
+function calculateMonthlyTotal();
+function assess20KExcess();
+function updateMonthlyTotal();
