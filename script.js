b52f31403dfa2b7575a893a3966030ad9d22f78e
allthethings <learn2code@respectdesfonds.com>
2020-03-14 23:54:06 -0500

    testing push of staff to array

--- script.js
+++ script.js
@@ -1,10 +1,72 @@
-console.log("js");
+console.log('js');
+const staff = [];
+
+const clair = {
+    lastName: 'Voyant',
+    firstName: 'Clair',
+    empID: 2845,
+    jobTitle: 'Department Head',
+    salary: 90000  
+};
+
+const ima = {
+    lastName: 'Student',
+    firstName: 'Ima',
+    empID: 2322,
+    jobTitle: 'Supervisor',
+    salary: 72000  
+};
+
+// test push to staff array
+staff.push(clair, ima);
+console.log(staff);
 
 
-function collect();
-function store();
-function clearInput();
-function append();
-function calculateMonthlyTotal();
-function assess20KExcess();
-function updateMonthlyTotal();
+
+
+
+
+// function collect() {
+// const employee = {
+// lastName: $(document.getElementsByName.lastName),
+// firstName: $(document.getElementsByName.firstName),
+// empID: $(document.getElementsByName.empID),
+// jobTitle: $(document.getElementsByName.jobTitle),
+// salary: $(document.getElementsByName.salary)
+// };
+// staff.push(employee)
+// }
+
+// for 
+
+
+// // test collect
+// collect ()
+// console.log(staff);
+
+// function store() {
+// employees.push();
+// };
+
+// function clearInput() {
+
+// };
+
+// function append() {
+
+// };
+
+// function calculateMonthlyTotal() {
+//     for (let i = 0; i < staff.length; i++) {
+//         console.log(staff.lastName);
+//     }
+
+// };
+
+// function assess20KExcess() {
+
+// };
+
+// function updateMonthlyTotal() {
+
+// };
