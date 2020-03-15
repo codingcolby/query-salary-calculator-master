5b53418338d656129748f536a052eaa0e112e48f
allthethings <learn2code@respectdesfonds.com>
2020-03-15 14:51:06 -0500

    verifying add employee button functionality

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 console.log('js');
 const staff = [];
+$(document).ready(eomReady);
 
+function eomReady() {
+// event listening
+    $('.js-input-addbtn').on('click', salariesDue);
+    $('.js-output-delbtn').on('click', deleteStaffEntry );
+
+}
+
+// test push to staff array
 const clair = {
     lastName: 'Voyant',
     firstName: 'Clair',
@@ -17,52 +26,67 @@ const ima = {
     salary: 72000
 };
 
-// test push to staff array
 staff.push(clair, ima);
 console.log(staff);
 
+function deleteStaffEntry(event) {
+    console.log('Delete ', event);
+    console.log('Delete ', this);
+    const staffIndex = $(this).data('index');
+    console.log('Delete ', staffIndex);
+    staff.splice(staffIndex, 1);
 
-// function collect() {
-// const employee = {
-// lastName: $(document.getElementsByName.lastName),
-// firstName: $(document.getElementsByName.firstName),
-// empID: $(document.getElementsByName.empID),
-// jobTitle: $(document.getElementsByName.jobTitle),
-// salary: $(document.getElementsByName.salary)
-// };
-// staff.push(employee)
-// }
+    updateDisplay();
+}
 
-// for () {}
+function salariesDue(event) {
+console.log('Who needs to be paid');
+const employee = {
+firstName: $('.js-input-fName').val(),
+lastName: $('.js-input-lName').val(),
+empID: $('.js-input-eID').val(),
+jobTitle: $('.js-input-title').val(),
+salary: $('.js-input-sal').val(),
+};
 
+staff.push(employee);
+console.log('Salaries Due: ', staff);
+updateDisplay();
+}
 
-// // test collect
-// collect ()
-// console.log(staff);
+function updateDisplay() {
+    $('.js-tableRows').empty();
 
-// function store() {
-// employees.push();
-// };
+    let monthlyTotal = 0;
+
+    for (let i = 0; i < staff.length; i++) {
+
+        const staffPerson = staff[i];
+        monthlyTotal += (staffPerson.salary * 1);
+
+    $('.js-tableRows').append(`
+    <tr>
+        <td>${staffPerson.firstName}</td>
+        <td>${staffPerson.lastName}</td>
+        <td>${staffPerson.empID}</td>
+        <td>${staffPerson.jobTitle}</td>
+        <td>${staffPerson.salary}</td>
+        <td><button class='js-output-delbtn' data-index='${i}'>Delete</button></td>
+    </tr>
+    `);
+}
+
+$('.js-amountDueToStaff').text(`$${monthlyTotal}`);
+
+}
 
 // function clearInput() {
 
 // };
 
-// function append() {
-
-// };
-
-// function calculateMonthlyTotal() {
-//     for (let i = 0; i < staff.length; i++) {
-//         console.log(staff.lastName);
-//     }
-
-// };
 
 // function assess20KExcess() {
 
 // };
 
-// function updateMonthlyTotal() {
 
-// };
